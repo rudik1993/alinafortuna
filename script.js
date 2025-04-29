27eda82e0d1e6aea5e078f5ae2b8c44a551955dc
rudik1993 <timofej.telesev@gmail.com>
2025-04-29 12:57:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    // Календарь с FullCalendar
     $('#calendar').fullCalendar({
         events: [
-            { title: 'Сеанс 1', start: '2025-05-01T10:00:00', end: '2025-05-01T11:00:00' },
-            { title: 'Сеанс 2', start: '2025-05-01T14:00:00', end: '2025-05-01T15:00:00' }
+            { title: 'Сеанс 1', start: '2025-04-30T10:00:00', end: '2025-04-30T11:00:00', description: 'Доступно для записи' },
+            { title: 'Сеанс 2', start: '2025-04-30T14:00:00', end: '2025-04-30T15:00:00', description: 'Доступно для записи' }
         ],
         eventClick: function(event) {
             alert('Вы выбрали сеанс: ' + event.title + ' в ' + event.start.format('HH:mm'));
@@ -12,9 +11,5 @@ $(document).ready(function() {
         }
     });
 
-    // Анимации с AOS
-    AOS.init({
-        duration: 1500, // время анимации
-        once: true // анимации только один раз
-    });
+    AOS.init({ duration: 1000, once: true });
 });
