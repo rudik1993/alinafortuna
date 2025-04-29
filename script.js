c410c9af77df4c0e8b626941625346a01b87d8b2
rudik1993 <timofej.telesev@gmail.com>
2025-04-29 10:19:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(() => {
+        document.querySelector('.animation').style.display = 'none';
+    }, 3000);  // Wait 3 seconds for the animation to finish
+});
